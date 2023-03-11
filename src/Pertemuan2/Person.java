0cbea5c7bf0de994266d91f2734dd949ec84de1a
mhdri <mhdri@mrifkiferdian>
2023-03-11 11:50:02 +0700

    Pertemuan 2

---
+++ src/Pertemuan2/Person.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pertemuan2;
+
+/**
+ *
+ * @author mhdri
+ */
+ public class Person {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+    
+    public String getNAme(){
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+}
